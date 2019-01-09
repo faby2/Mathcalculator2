8c6d6fa0d64398a981a91efd1e00bb0624835b07
fabrice <fabricerav@gmail.com>
2019-01-09 11:09:47 +0100

    rectif

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -11,32 +11,31 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class MathCalculator extends CordovaPlugin {
-
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException 
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if(action.equals("add")){
-            this.add(args, callbackContext);
-            return true;
-        }else if(action.equals("substract")){
-            this.substract(args, callbackContext);
-            return true;
-        }
-        return false;
+        this.add(args, callbackContext);
+        return true;
+    }else if(action.equals("substract")){
+        this.substract(args, callbackContext);
+        return true;
     }
+        return false;
+}
 
- 
+
 
     private void add(JSONArray args, CallbackContext callback) {
         if(args != null) {
             try{
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success("", (p1+p2));
+                callback.success(""+ (p1+p2));
             }catch(Exception ex) {
-                callback.erro("something went " + ex);
+                callback.error("something went " + ex);
             }
         }else {
-            callback.erro("please donot pass null value");
+            callback.error("please donot pass null value");
         }
     }
 
@@ -45,12 +44,12 @@ public class MathCalculator extends CordovaPlugin {
             try{
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success("", (p1+p2));
+                callback.success("" + (p1+p2));
             }catch(Exception ex) {
-                callback.erro("something went " + ex);
+                callback.error("something went " + ex);
             }
         }else {
-            callback.erro("please donot pass null value");
+            callback.error("please donot pass null value");
         }
     }
 }
